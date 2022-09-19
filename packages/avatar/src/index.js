0f6775ac8491568f8f28ce2bf1d63dc646019233
Harsh <harshgupta.harry@gmail.com>
2022-09-19 10:40:18 +0530

    stage avatar

--- packages/avatar/src/index.js
+++ packages/avatar/src/index.js
@@ -2,15 +2,15 @@
 
 import React from 'react';
 import tokens from 'go-design-system-theme';
-import { Avatar, AvatarGroup } from '@chakra-ui/avatar';
-export function Button(props) {
+import { AvatarNPM, AvatarGroup } from '@chakra-ui/avatar';
+export function Avatar(props) {
   return (
     <AvatarGroup size="md" max={2}>
-      <Avatar name="Ryan Florence" src="https://bit.ly/ryan-florence" />
-      <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
-      <Avatar name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
-      <Avatar name="Prosper Otemuyiwa" src="https://bit.ly/prosper-baba" />
-      <Avatar name="Christian Nwamba" src="https://bit.ly/code-beast" />
+      <AvatarNPM name="Ryan Florence" src="https://bit.ly/ryan-florence" />
+      <AvatarNPM name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
+      <AvatarNPM name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
+      <AvatarNPM name="Prosper Otemuyiwa" src="https://bit.ly/prosper-baba" />
+      <AvatarNPM name="Christian Nwamba" src="https://bit.ly/code-beast" />
     </AvatarGroup>
   );
 }
