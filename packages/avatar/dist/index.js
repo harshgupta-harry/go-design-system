19ef858cd4b46bdede6248993b3fcb12e4140bee
Harsh <harshgupta.harry@gmail.com>
2022-09-19 13:28:33 +0530

    stage

--- packages/avatar/dist/index.js
+++ packages/avatar/dist/index.js
@@ -2,26 +2,45 @@ import React from 'react';
 import 'go-design-system-theme';
 import { AvatarGroup, Avatar as Avatar$1 } from '@chakra-ui/avatar';
 
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
+
 function Avatar(props) {
-  return React.createElement(AvatarGroup, {
+  const {
+    avatars = [],
+    max = 2,
+    avatarGroupProps = {},
+    avatarProps = {}
+  } = props;
+
+  const renderAvatars = () => {
+    return avatars.map((avatar, index) => {
+      return React.createElement(Avatar$1, _extends({
+        name: avatar.name,
+        src: avatar.url
+      }, avatarProps));
+    });
+  };
+
+  return React.createElement(AvatarGroup, _extends({
     size: "md",
-    max: 2
-  }, React.createElement(Avatar$1, {
-    name: "Ryan Florence",
-    src: "https://bit.ly/ryan-florence"
-  }), React.createElement(Avatar$1, {
-    name: "Segun Adebayo",
-    src: "https://bit.ly/sage-adebayo"
-  }), React.createElement(Avatar$1, {
-    name: "Kent Dodds",
-    src: "https://bit.ly/kent-c-dodds"
-  }), React.createElement(Avatar$1, {
-    name: "Prosper Otemuyiwa",
-    src: "https://bit.ly/prosper-baba"
-  }), React.createElement(Avatar$1, {
-    name: "Christian Nwamba",
-    src: "https://bit.ly/code-beast"
-  }));
+    max: max
+  }, avatarGroupProps), renderAvatars());
 }
 
 export { Avatar };
--- packages/avatar/src/index.js
+++ packages/avatar/src/index.js
@@ -4,13 +4,21 @@ import React from 'react';
 import tokens from 'go-design-system-theme';
 import { Avatar as AvatarNPM, AvatarGroup } from '@chakra-ui/avatar';
 export function Avatar(props) {
+  const {
+    avatars = [],
+    max = 2,
+    avatarGroupProps = {},
+    avatarProps = {},
+  } = props;
+
+  const renderAvatars = () => {
+    return avatars.map((avatar, index) => {
+      return <AvatarNPM name={avatar.name} src={avatar.url} {...avatarProps} />;
+    });
+  };
   return (
-    <AvatarGroup size="md" max={2}>
-      <AvatarNPM name="Ryan Florence" src="https://bit.ly/ryan-florence" />
-      <AvatarNPM name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
-      <AvatarNPM name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
-      <AvatarNPM name="Prosper Otemuyiwa" src="https://bit.ly/prosper-baba" />
-      <AvatarNPM name="Christian Nwamba" src="https://bit.ly/code-beast" />
+    <AvatarGroup size="md" max={max} {...avatarGroupProps}>
+      {renderAvatars()}
     </AvatarGroup>
   );
 }
